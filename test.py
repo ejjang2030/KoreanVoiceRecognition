947ee22ac680181f3abfaf8484bed316390c2f6e
ejjang2030 <ejjang2030@gmail.com>
2021-03-05 02:02:24 +0900

    2021.03.05-02:00 : 데이터 변환 및 정제 솔트룩스에서 음성인식으로 Http GET해온 content string을 list로 파일 변환

--- test.py
+++ test.py
@@ -2,14 +2,19 @@ import requests
 import json
 import base64
 from apikey import saltlux_api_key as api
+import re
 
 audio = open("sample_data_64kbps.mp3", 'rb')
 
+# audio = open("sample_data_64kbps.mp3", 'rb')
+
+# audio = open("sample_data_64kbps.wav", 'rb')  # wav 파일이며 sample data가 5MB 이상이다.
+# 그래서 error_code는 ERROR_UNKOWN_SAMPLERATE_FILE
+
 audio_content = audio.read()
 encoded_audio = base64.b64encode(audio_content).decode("utf-8")
-
+# print(encoded_audio)
 audio.close()
-# youtube = pyglet.media.load('sample_data_64kbps.mp3')
 
 # URL 지정
 url = api["url"]
@@ -28,5 +33,13 @@ params = {
 
 response = requests.post(url, headers=headers, data=json.dumps(params))
 
-print(response.content)
-
+jsonified_string = response.json()["content"]
+# print(type(jsonified))
+# print(jsonified_string)
+list_str = jsonified_string.split('\n')
+container = []
+for c in list_str:
+    con = re.split(" +", c)
+    del con[0]
+    container.append(con)
+print(container)
